44bef4826265dd50678e38f2eb515ab1412cb15d
marcosribeirodacunha <mrcmarcos100@gmail.com>
2021-02-18 15:46:58 -0300

    Setup styled-components and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <Footer />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <Footer />
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from 'styled-components';
+import { darken, lighten } from 'polished';
+
+export default createGlobalStyle`
+
+  :root {
+    font-size: 62.5%;
+
+    --font-family-heading: 'Montserrat', sans-serif;
+    --font-family-body: 'B612', sans-serif;
+
+    --font-display-1: 900 7.2rem/1 var(--font-family-heading);
+    --font-display-2: 900 4.8rem/1.2 var(--font-family-heading);
+    --font-headline: 600 3.6rem/1.2 var(--font-family-heading);
+
+    // Apply these in the mobile breakpoint
+    // --font-display-1: 900 4.8rem/1 var(--font-family-heading);
+    // --font-display-2: 900 3.6rem/1.2 var(--font-family-heading);
+    // --font-headline: 600 2.8rem/1.2 var(--font-family-heading);
+
+    --font-subheading-1: 600 2.4rem/1.2 var(--font-family-heading);
+    --font-subheading-2: 600 2.1rem/1.2 var(--font-family-heading);
+    --font-subheading-3: 600 1.8rem/1.2 var(--font-family-heading);
+
+    --font-paragraph-big: 400 1.8rem/1.5 var(--font-family-body);
+    --font-paragraph: 400 1.6rem/1.5 var(--font-family-body);
+    --font-paragraph-bold: 700 1.6rem/1.5 var(--font-family-body);
+    --font-paragraph-small: 400 1.4rem/1.5 var(--font-family-body);
+
+    --font-button: 700 1.6rem/1.5 var(--font-family-body);
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, body, #root {
+    height: 100vh;
+  }
+
+  body {
+    background: ${({ theme }) => lighten(0.35, theme.colors.silver_base)};
+  }
+
+  body, input, textarea {
+    font: var(--font-paragraph);
+    color: ${({ theme }) => theme.colors.gray_base};
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    color: ${({ theme }) => darken(0.15, theme.colors.gray_base)};
+    font-family: var(--font-family-heading);
+  }
+
+`;
