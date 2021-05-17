0bbc13425eac3b064a2b0ad94ab338d1dbdca21e
Marcos RC <mrcmarcos100@gmail.com>
2021-05-17 18:03:55 -0300

    Add mailboxes for WorkingUS, Questions, Archived and Deleted

--- src/pages/private/Mailbox/Archived/index.tsx
+++ src/pages/private/Mailbox/Archived/index.tsx
@@ -5,67 +5,67 @@ import { MailboxProvider } from '../../../../context/MailboxContext';
 const MESSAGES = [
   {
     id: '1',
-    name: 'Candidato',
+    name: 'Pessoa',
     email: 'email@email.com',
     phone: '24988776655',
-    subject: 'Um assunto caso tenha',
+    subject: 'Qual a sua dúvida',
     message:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'https://linkedin.com',
+    linkedin: '',
     contacted: false,
     read: false,
   },
   {
     id: '2',
-    name: 'Candidato 2',
+    name: 'Pessoa 2',
     email: 'email@email.com',
     phone: '24988776655',
-    subject: 'Um assunto caso tenha',
+    subject: 'Qual a sua dúvida',
     message:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'http://linkedin.com',
+    linkedin: '',
     contacted: false,
     read: false,
   },
   {
     id: '3',
-    name: 'Candidato 3',
+    name: 'Pessoa 3',
     email: 'email@email.com',
     phone: '24988776655',
-    subject: 'Um assunto caso tenha',
+    subject: 'Qual a sua dúvida',
     message:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'http://linkedin.com',
+    linkedin: '',
     contacted: true,
     read: true,
   },
   {
     id: '4',
-    name: 'Candidato 4',
+    name: 'Pessoa 4',
     email: 'email@email.com',
     phone: '24988776655',
-    subject: 'Um assunto caso tenha',
+    subject: 'Qual a sua dúvida',
     message:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'http://linkedin.com',
+    linkedin: '',
     contacted: false,
     read: false,
   },
   {
     id: '5',
-    name: 'Candidato 5',
+    name: 'Pessoa 5',
     email: 'email@email.com',
     phone: '24988776655',
-    subject: 'Um assunto caso tenha',
+    subject: 'Qual a sua dúvida',
     message:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'http://linkedin.com',
+    linkedin: '',
     contacted: false,
     read: true,
   },
 ];
 
-const WorkingUsMailbox: React.FC = () => {
+const Archived: React.FC = () => {
   return (
     <MailboxProvider messages={MESSAGES}>
       <div>
@@ -76,4 +76,4 @@ const WorkingUsMailbox: React.FC = () => {
   );
 };
 
-export default WorkingUsMailbox;
+export default Archived;
--- src/routes/mailbox.routes.tsx
+++ src/routes/mailbox.routes.tsx
@@ -2,6 +2,9 @@ import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
 
 import WorkingUsMailbox from '../pages/private/Mailbox/WorkingUs';
 import EnrollMailbox from '../pages/private/Mailbox/Enroll';
+import QuestionsMailbox from '../pages/private/Mailbox/Questions';
+import ArchivedMailbox from '../pages/private/Mailbox/Archived';
+import DeletedMailbox from '../pages/private/Mailbox/Deleted';
 
 const MailboxRoutes = () => {
   const { path } = useRouteMatch();
@@ -10,6 +13,9 @@ const MailboxRoutes = () => {
     <Switch>
       <Route path={`${path}/matriculas`} component={EnrollMailbox} />
       <Route path={`${path}/trabalhe-conosco`} component={WorkingUsMailbox} />
+      <Route path={`${path}/fale-conosco`} component={QuestionsMailbox} />
+      <Route path={`${path}/arquivados`} component={ArchivedMailbox} />
+      <Route path={`${path}/excluidos`} component={DeletedMailbox} />
 
       <Route component={() => <Redirect to={`${path}/matriculas`} />} />
     </Switch>
