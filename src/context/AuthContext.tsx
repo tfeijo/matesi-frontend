e6840ae31e682b18eedaa66245736d13fe53e58d
Marcos RC <mrcmarcos100@gmail.com>
2021-05-20 11:51:00 -0300

    Add and setup AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,63 @@
+import { createContext, useCallback, useEffect, useState } from 'react';
+import usePersistedState from '../hooks/usePersistedState';
+import api from '../services/api';
+
+type Credentials = {
+  email: string;
+  password: string;
+};
+
+type User = {
+  name: string;
+  email: string;
+};
+
+interface AuthContextValue {
+  isSigned: boolean;
+  isLoading: boolean;
+  user: User | null;
+  signIn: (credentials: Credentials) => Promise<void>;
+  signOut: () => void;
+}
+
+const AuthContext = createContext<AuthContextValue>({} as AuthContextValue);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = usePersistedState<User | null>('@matesi:user', null);
+  const [token, setToken] = usePersistedState('@matesi:token', '');
+
+  useEffect(() => {
+    if (user && token) {
+      api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+
+    setIsLoading(false);
+  }, [token, user]);
+
+  const signIn = useCallback(
+    async (credentials: Credentials) => {
+      const { data } = await api.post('sessions', credentials);
+
+      setUser(data.user);
+      setToken(data.token);
+    },
+    [setToken, setUser],
+  );
+
+  const signOut = useCallback(() => {
+    setUser(null);
+    setToken('');
+    api.defaults.headers.authorization = undefined;
+  }, [setToken, setUser]);
+
+  return (
+    <AuthContext.Provider
+      value={{ isSigned: !!user, isLoading, user, signIn, signOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
