91e8b2ec24d5ea85ef05f066aa8ce4f2f2188f44
Marcos RC <mrcmarcos100@gmail.com>
2021-05-20 11:52:10 -0300

    Setup authentication in Route component

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { RouteProps, Route, RouteComponentProps } from 'react-router-dom';
+import {
+  RouteProps,
+  Route,
+  RouteComponentProps,
+  Redirect,
+} from 'react-router-dom';
 import Footer from '../components/Footer';
+import Loader from '../components/Loader';
+import useAuth from '../hooks/useAuth';
 
 interface IProps extends RouteProps {
   isAdmin?: boolean;
@@ -11,12 +18,23 @@ interface IProps extends RouteProps {
 
 const RouteWrapper: React.FC<IProps> = ({
   component: Component,
+  path,
   isAdmin = false,
   ...rest
 }) => {
+  const { isLoading, isSigned } = useAuth();
+
+  if (isLoading) return <Loader size={48} style={{ height: '100vh' }} />;
+
+  if (!isSigned && isAdmin) return <Redirect to="/entrar" />;
+
+  if (isSigned && path === '/entrar')
+    return <Redirect to="/mensagens/matriculas" />;
+
   return (
     <Route
       {...rest}
+      path={path}
       render={props => (
         <>
           <Component {...props} />
