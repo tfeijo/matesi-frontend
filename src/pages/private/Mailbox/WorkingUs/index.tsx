432ac8c604336e63b79ca0081d06a61c9f0f2146
Marcos RC <mrcmarcos100@gmail.com>
2021-05-21 12:07:18 -0300

    Add fetch to list all messages for working us

--- src/pages/private/Mailbox/WorkingUs/index.tsx
+++ src/pages/private/Mailbox/WorkingUs/index.tsx
@@ -1,73 +1,57 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import Loader from '../../../../components/Loader';
 import MailDetail from '../../../../components/Mailbox/MailDetail';
 import MailList from '../../../../components/Mailbox/MailList';
 import { MailboxProvider } from '../../../../context/MailboxContext';
+import api from '../../../../services/api';
 
-const MESSAGES = [
-  {
-    id: '1',
-    name: 'Candidato',
-    email: 'email@email.com',
-    phone: '24988776655',
-    subject: 'Um assunto caso tenha',
-    message:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'https://linkedin.com',
-    contacted: false,
-    read: false,
-  },
-  {
-    id: '2',
-    name: 'Candidato 2',
-    email: 'email@email.com',
-    phone: '24988776655',
-    subject: 'Um assunto caso tenha',
-    message:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'http://linkedin.com',
-    contacted: false,
-    read: false,
-  },
-  {
-    id: '3',
-    name: 'Candidato 3',
-    email: 'email@email.com',
-    phone: '24988776655',
-    subject: 'Um assunto caso tenha',
-    message:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'http://linkedin.com',
-    contacted: true,
-    read: true,
-  },
-  {
-    id: '4',
-    name: 'Candidato 4',
-    email: 'email@email.com',
-    phone: '24988776655',
-    subject: 'Um assunto caso tenha',
-    message:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'http://linkedin.com',
-    contacted: false,
-    read: false,
-  },
-  {
-    id: '5',
-    name: 'Candidato 5',
-    email: 'email@email.com',
-    phone: '24988776655',
-    subject: 'Um assunto caso tenha',
-    message:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur error reiciendis ut quis repudiandae eaque quaerat? Accusantium fugiat laboriosam blanditiis.',
-    linkedin: 'http://linkedin.com',
-    contacted: false,
-    read: true,
-  },
-];
+interface Message {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+  read: boolean;
+  contacted: boolean;
+}
+
+type MessageResponse = Omit<Message, 'read' | 'contacted' | 'message'> & {
+  isRead: boolean;
+  isContact: boolean;
+  about_me: string;
+};
 
 const WorkingUsMailbox: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    async function loadMessages() {
+      const { data } = await api.get<MessageResponse[]>('work_with_us');
+
+      const formattedData = data.map(
+        ({ isContact, isRead, about_me, ...rest }) => ({
+          read: isRead,
+          contacted: isContact,
+          message: about_me,
+          ...rest,
+        }),
+      );
+
+      setMessages(formattedData);
+    }
+
+    try {
+      loadMessages();
+    } catch (error) {
+      toast.error('Um erro inesperado ocorreu. Por favor, tente novamente.');
+    }
+  }, []);
+
+  if (messages.length === 0) return <Loader size={48} />;
+
   return (
-    <MailboxProvider messages={MESSAGES}>
+    <MailboxProvider messages={messages} boxName="work_with_us">
       <div>
         <MailList />
         <MailDetail />
